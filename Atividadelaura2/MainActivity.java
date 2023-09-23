072df4c123cbe3286770de092faaf883a1b0df66
Laura nascente <92733398+itsmelaura07@users.noreply.github.com>
2023-09-23 01:30:19 -0300

    Calculolaura

---
+++ Atividadelaura2/MainActivity.java
@@ -0,0 +1,9 @@
+
+double num1, num2, num3, res;
+
+@override
+protec void emCreate(Bundle savedInstancesState) {
+    super.oncreate(saveInstances);
+    setConstantView(R.layout.activity_main);
+
+}
